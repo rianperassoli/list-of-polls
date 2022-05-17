55eef223b7a4d542cf47baa5429bac4c991c36ac
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-17 15:07:44 -0300

    Created a simple Footer and Header from the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
 <main>
 	<h1>Hello World!</h1>
 </main>
+<Footer />
 
 <style>
-	
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
